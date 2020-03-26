e791a347b0d74acecf2af5a591f6ff78ab6ed886
Sam Thorogood <sam.thorogood@gmail.com>
2020-03-27 01:32:58 +1100

    zero commit

---
+++ index.js
@@ -0,0 +1,108 @@
+import path from 'path';
+
+/**
+ * Wraps a filesystem path and provides helpers to change its parts.
+ */
+export default class FileParts {
+  #path;
+
+  /**
+   * @param {(string|!FileParts)=} raw
+   */
+  constructor(raw = '') {
+    if (raw instanceof FileParts) {
+      this.#path = raw.path;
+    } else {
+      this.#path = raw.toString('utf-8');
+    }
+  }
+
+  toString() {
+    return this.#path;
+  }
+
+  /**
+   * @return {string} returns the whole path
+   */
+  get path() {
+    return this.#path;
+  }
+
+  /**
+   * @param {string} v sets the whole path
+   */
+  set path(v) {
+    this.#path = v;
+  }
+
+  /**
+   * @return {string} return the basename without extension
+   */
+  get name() {
+    return path.basename(this.#path, this.extname);
+  }
+
+  /**
+   * @param {string} v updates the basename ignoring extension
+   */
+  set name(v) {
+    this.path = path.join(this.dirname, v + this.extname);
+  }
+
+  /**
+   * @return {string} return the basename without extension
+   */
+  get stem() {
+    return this.name;
+  }
+
+  /**
+   * @param {string} v updates the basename ignoring extension
+   */
+  set stem(v) {
+    this.name = v;
+  }
+
+  /**
+   * @return {string} the directory, will be '.' if unknown/unset
+   */
+  get dirname() {
+    return path.dirname(this.#path);
+  }
+
+  /**
+   * @param {string} v sets the directory, is not normalized
+   */
+  set dirname(v) {
+    this.path = path.join(v, this.basename);
+  }
+
+  /**
+   * @return {string} the basename, may be empty for dir
+   */
+  get basename() {
+    return path.basename(this.#path);
+  }
+
+  /**
+   * @param {string} v the basename to set
+   */
+  set basename(v) {
+    this.path = path.join(this.dirname, v);
+  }
+
+  /**
+   * @return {string} the extension including the dot
+   */
+  get extname() {
+    return path.extname(this.#path);
+  }
+
+  /**
+   * @param {string} v the extension to set, must include dot (or will just append)
+   */
+  set extname(v) {
+    const {dir, name} = path.parse(this.#path);
+    this.path = path.join(dir, name + v);
+  }
+}
---
+++ test.js
@@ -0,0 +1,68 @@
+import mocha from 'mocha';
+import chai from 'chai';
+import FileParts from './index.js';
+
+const {suite, test} = mocha;
+const {assert} = chai;
+
+suite('FileParts', () => {
+  test('empty', () => {
+    const p = new FileParts();
+    assert.equal(p.name, '');
+    assert.equal(p.basename, '');
+    assert.equal(p.extname, '');
+    assert.equal(p.dirname, '.');
+    assert.equal(p.stem, '');
+  });
+
+  test('name', () => {
+    const p = new FileParts('foo');
+    assert.equal(p.name, 'foo');
+    assert.equal(p.basename, 'foo');
+
+    p.extname = '.bar';
+    assert.equal(p.name, 'foo');
+    assert.equal(p.basename, 'foo.bar');
+
+    p.extname = 'foo';
+    assert.equal(p.name, 'foofoo');
+    assert.equal(p.basename, 'foofoo');
+
+    p.extname = '.foo.bar';
+    assert.equal(p.name, 'foofoo.foo');
+    assert.equal(p.basename, 'foofoo.foo.bar');
+    assert.equal(p.extname, '.bar');
+  });
+
+  test('dir', () => {
+    const p = new FileParts('foo');
+
+    p.dirname = '../hello';
+    assert.equal(p.path, '../hello/foo');
+    assert.equal(p.dirname, '../hello');
+
+    p.dirname = '../zing/'
+    assert.equal(p.path, '../zing/foo');
+    assert.equal(p.dirname, '../zing');
+
+    p.dirname = '';
+    assert.equal(p.path, 'foo');
+    assert.equal(p.dirname, '.');
+    assert.equal(p.basename, 'foo');
+
+    // This is how Node says it works.
+    p.path = 'foo/bar/';
+    assert.equal(p.dirname, 'foo');
+    assert.equal(p.basename, 'bar');
+  });
+
+  test('clone', () => {
+    const p = new FileParts('/test/path.zip');
+    const p2 = new FileParts(p);
+
+    p.dirname = 'foo';
+    assert.equal(p2.dirname, '/test');
+    assert.equal(p.dirname, 'foo');
+  });
+});
+
